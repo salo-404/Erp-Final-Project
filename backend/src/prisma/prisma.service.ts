8f4922368a0a19dcbe4b4f770d7f86b76fd1bbca
Joseph-CH7 <joe.chahine20057@gmail.com>
2026-08-15 21:17:01 +0300

    Complete products module

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -2,6 +2,8 @@ import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
+import 'dotenv/config';
+
 @Injectable()
 export class PrismaService
   extends PrismaClient
